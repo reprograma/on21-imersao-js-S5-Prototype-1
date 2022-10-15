fe6772f14ee7d440dd11ea7512324eb53cd433c4
aline.viana <aline.viana@cloud2go.com.br>
2022-10-15 11:27:17 -0300

    feat:  change original value

--- exercicios/para-sala/exercicio-2/resolucao/resolucao.js
+++ exercicios/para-sala/exercicio-2/resolucao/resolucao.js
@@ -5,8 +5,21 @@ function Employee(firstName, lastName, salary) {
     employee.firstName = firstName;
     employee.lastName = lastName;
     employee.salary = salary; 
+    employee.raiseSalary = (percentage) => {
+        const increseOfSalary = employee.salary * percentage;
+        const newSalary = employee.salary + increseOfSalary;
+
+        employee.salary = newSalary
+
+        return newSalary
+    }
 
     return employee;
 }
 
-console.log(Employee('Igor', 'Viana', '14000'))
+const igorViana = Employee('Igor', 'Viana', 14000)
+console.log(igorViana)
+igorViana.raiseSalary(0.1)
+console.log(igorViana)
+
+

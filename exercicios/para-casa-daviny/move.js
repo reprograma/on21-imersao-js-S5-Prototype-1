9e21fef12ea626fd4af73ab68c68bdbace679580
Daviny Vidal <daviny@vidal.dev.br>
2022-10-20 20:38:36 -0300

    add questions

--- exercicios/para-casa-daviny/move.js
+++ exercicios/para-casa-daviny/move.js
@@ -1,16 +1,15 @@
 const Type = require('./type')
 
-function Move (name, type) {
+function Move (name, type_name, type_damage_dealt, type_damage_taken, power, accuracy, target, priority) {
+    const obj = Object.create(Move.prototype)
     this.name = name
-    this.type = Type(type.name, type.damage_dealt, type.damage_taken)
+    this.type = {name: type_name, damage_dealt: type_damage_dealt, damage_taken: damage_taken}
+
     // this.summary = summary
     // this.power = power
     }
 
-const bug = new Move('Overgrow', ['bug', 
-[{name: 'bug', value: 0.5}, { name: 'dark', value: 1 }], 
-[{ name: 'bug', value: 2 }, { name: 'dark', value: 1 }]
-])
+const bug = new Move('Overgrow', 'bug',  [{name: 'bug', value: 0.5}, { name: 'dark', value: 1 }], [{ name: 'bug', value: 2 }, { name: 'dark', value: 1 }], )
 
 console.log(bug)
 

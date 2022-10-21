ab24d989ce61c03d9670c22a9df1ae00c63e480e
Priscila Ferreira de Sousa <ferreiradesousap@gmail.com>
2022-10-21 20:34:56 -0300

    Adicionando prototype ao exercício para criação do pokemon

--- exercicios/para-casa/entregas/priscila-ferreira-de-sousa/exercicioParaCasa.js
+++ exercicios/para-casa/entregas/priscila-ferreira-de-sousa/exercicioParaCasa.js
@@ -1,26 +1,6 @@
 //exercício incompleto
 
-const pokemonMethods = {
-    atack: function atack (move, attackedPokemon, attackingPokemon){
-        let damageSum = 0;
-         move.type.damage_dealts.array.forEach(d => {
-            damageSum += d.value;
-        });
-        if(!attackingPokemon.types.includes(move.type)){
-            throw `O pokemón ${attackingPokemon.name} não pode realizar movimentos do tipo ${move.type}`; 
-        }        
-        console.log(`O pokemón ${attackingPokemon.name} usou o movimento ${move.name}`);
-
-        if(attackedPokemon.stats.hp <= damageSum){
-            console.log(`O pokémon ${attackedPokemon.name} foi derrotado.`);
-        }else{
-            attackedPokemon.stats.hp -= damageSum;
-            console.log(`O pokemón ${attackedPokemon.name} sofreu ${damageSum} de dano.`);
-        }
-    }
-}
-
-function DamageType(name, value){
+function DamageType(name, value) {
     const damageType = {};
     damageType.name = name;
     damageType.value = value;
@@ -28,7 +8,7 @@ function DamageType(name, value){
     return damageType;
 }
 
-function Type(name, damage_dealts, damacge_taken) { //move type
+function Type(name, damage_dealts, damage_taken) { //move type
     const type = {};
     type.name = name;
     type.damage_dealts = damage_dealts; //array de DamageTypes
@@ -51,15 +31,15 @@ function Move(name, type, summary, power, target, accuracy, priority) {
     move.name = name;
     move.type = type; //Type
     move.summary = summary;
-    move. power = power;
+    move.power = power;
     move.target = target; //  ('self', 'allies', 'selected')
     move.accuracy = accuracy;
     move.priority = priority;
-    
+
     return move;
 }
 
-function Stats (hp, attack, defense, specialAtack, specialDefense, speed){
+function Stats(hp, attack, defense, specialAtack, specialDefense, speed) {
     const stats = {};
     stats.hp = hp;
     stats.attack = attack;
@@ -73,19 +53,34 @@ function Stats (hp, attack, defense, specialAtack, specialDefense, speed){
 
 function Pokemon(name, number, types, abilities, hidden_ability, stats,
     evolution_stage, level, evolution_level, exp, nickname) {
-    const pokemon = {};
+    let pokemon = Object.create(Pokemon.prototype);
     pokemon.name = name;
     pokemon.number = number;
     pokemon.types = types;  //array de Type
     pokemon.abilities = abilities;   //array de Abilities
     pokemon.hidden_ability = hidden_ability; //Ability
     pokemon.stats = stats; //Stats
-    pokemon.evolution_stage = evolution_stage ;  // ('baby', 'basic', 'stage-1', 'stage-2')
+    pokemon.evolution_stage = evolution_stage;  // ('baby', 'basic', 'stage-1', 'stage-2')
     pokemon.level = level;
     pokemon.evolution_level = evolution_level;
     pokemon.exp = exp;
     pokemon.nickname = nickname;
-    pokemon.atack = pokemonMethods.atack;
+}
 
-    return pokemon;
+Pokemon.prototype.atack = function atack(move, attackedPokemon, attackingPokemon) {
+    let damageSum = 0;
+    move.type.damage_dealts.array.forEach(d => {
+        damageSum += d.value;
+    });
+    if (!attackingPokemon.types.includes(move.type)) {
+        throw `O pokemón ${attackingPokemon.name} não pode realizar movimentos do tipo ${move.type}`;
+    }
+    console.log(`O pokemón ${attackingPokemon.name} usou o movimento ${move.name}`);
+
+    if (attackedPokemon.stats.hp <= damageSum) {
+        console.log(`O pokémon ${attackedPokemon.name} foi derrotado.`);
+    } else {
+        attackedPokemon.stats.hp -= damageSum;
+        console.log(`O pokemón ${attackedPokemon.name} sofreu ${damageSum} de dano.`);
+    }
 }
